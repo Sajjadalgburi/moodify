32e07cf67cd0ee67a6f05638255fbd7120dd4c85
Sajjad Algburi <sajjadalgburi.dev@gmail.com>
2024-12-29 15:29:27 +0300

    imported header

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,10 @@
+import Header from "@/components/navigation/header";
 import React from "react";
 
 const Home = () => {
   return (
     <main>
+      <Header />
       <h1>Discover Spotify Songs That Match Your Mood with AI</h1>
     </main>
   );

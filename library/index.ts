1012018e7b1eb4cbf06cac0fae8409536d312e8d
Sajjad Algburi <sajjadalgburi.dev@gmail.com>
2024-12-29 15:24:46 +0300

    auth buttons as constants

--- library/index.ts
+++ library/index.ts
@@ -2,3 +2,12 @@
  * This file will contain all the library code / Constants.
  * Constants is code that won't be changed
  */
+
+export const authBtns = [
+  {
+    id: 1,
+    variant: "outline" as const,
+    text: "login",
+  },
+  { variant: "default" as const, text: "register", id: 2 },
+];

e67bc9da23fcf85fa5197cc632abd86d78e70d16
Sajjad Algburi <sajjadalgburi.dev@gmail.com>
2024-12-30 23:31:13 +0300

    randomness

--- app/page.tsx
+++ app/page.tsx
@@ -1,28 +1,27 @@
 import Footer from "@/components/footer/page";
 import LandingPage from "@/components/landing_page/page";
 import Header from "@/components/navigation/header";
+import { createClient } from "@/utils/supabase/client";
 import React from "react";
 
 const loggedIn = false;
 
-console.log("hello");
+const Home = async () => {
+  const supabase = await createClient();
 
-const Home = () => {
   return (
     <main className="overflow-auto min-h-screen">
-      <>
-        <Header />
-        {/* If user is not logged in then make sure to show initial page. 
-      Otherwise show logged in page */}
-        {loggedIn ? (
-          <div>
-            <h2>Logged In</h2>
-          </div>
-        ) : (
-          <LandingPage />
-        )}
-        <Footer />{" "}
-      </>
+      <Header />
+      {/* If user is not logged in then make sure to show initial page. 
+        Otherwise show logged in page */}
+      {loggedIn ? (
+        <div>
+          <h2>Logged In</h2>
+        </div>
+      ) : (
+        <LandingPage />
+      )}
+      <Footer />
     </main>
   );
 };

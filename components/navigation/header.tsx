e97d69e31625fc485d1b477f5926d40a8d34241e
BenLe26 <shockspvp123@gmail.com>
2024-12-30 13:41:24 -0700

    Added modal functionality to header

--- components/navigation/header.tsx
+++ components/navigation/header.tsx
@@ -4,12 +4,26 @@ import { Button } from "../ui/button";
 import { authBtns } from "@/library";
 import { useRouter } from "next/navigation";
 import { ModeToggle } from "./../theme-switch/page";
+import Link from "next/link";
+import { useSearchParams } from "next/navigation";
+import { createClient } from "@/utils/supabase/client";
 
 const Header = () => {
+  const supabase = createClient();
+
   const router = useRouter();
+  const searchParams = useSearchParams();
+  const openModal = searchParams.get("modal") as string | null;
+
+  if (openModal) {
+    return null;
+  }
+
   return (
     <header className="flex justify-between items-center px-4 py-2 relative bg-background z-50 overflow-hidden">
-      <div className="text-2xl font-bold">Logo</div>
+      <Link href="/" className="text-2xl font-bold">
+        Logo
+      </Link>
       <div className="gap-2 flex">
         <ModeToggle />
         {authBtns.map((btn) => (
@@ -17,7 +31,7 @@ const Header = () => {
             key={btn.id}
             onClick={() => router.push(`/auth?q=${btn.text}`)}
             variant={btn.variant}
-            className="font-bold"
+            className="font-bold capitalize"
           >
             {btn.text}
           </Button>

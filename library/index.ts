339b260dd4464699020f1837f0849cd62190bc16
BenLe26 <shockspvp123@gmail.com>
2024-12-30 13:41:40 -0700

    Formed data constants

--- library/index.ts
+++ library/index.ts
@@ -11,3 +11,24 @@ export const authBtns = [
   },
   { variant: "default" as const, text: "register", id: 2 },
 ];
+
+export const form_fields = [
+  {
+    id: "username",
+    type: "text",
+    label: "Username",
+    placeholder: "Enter your username",
+  },
+  {
+    id: "email",
+    type: "email",
+    label: "Email",
+    placeholder: "Enter your email",
+  },
+  {
+    id: "password",
+    type: "password",
+    label: "Password",
+    placeholder: "Enter your password",
+  },
+];
